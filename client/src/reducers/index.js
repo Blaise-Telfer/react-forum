cd98181e0344ca4d67c3e4c02ab9fbe2e2a01555
Blaise-Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2020-10-09 15:41:23 -0700

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,7 +3,6 @@ import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import { reducer as formReducer } from 'redux-form';
 
-
 export default combineReducers({
 	auth: authReducer,
 	errors: errorReducer,
