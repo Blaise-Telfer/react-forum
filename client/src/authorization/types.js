5ce00d30dbbc426557b0ca4d512a4a757e74a220
Blaise-Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2020-10-09 15:02:02 -0700

    Add files via upload

---
+++ client/src/authorization/types.js
@@ -0,0 +1,6 @@
+export const GET_ERRORS = "GET_ERRORS";
+export const GET_POSTS = "GET_POSTS";
+export const USER_LOADING = "USER_LOADING";
+export const POST_LOADING = "POST_LOADING";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const DELETE_POST = "DELETE_POST";
